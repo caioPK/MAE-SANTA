51604cd9f8d056db6e1c818cdf987c66894da259
caioPK <cai0_gabriel@hotmail.com>
2022-07-14 07:15:32 -0300

    first

---
+++ netlify/functions/test/test.js
@@ -0,0 +1,29 @@
+const axios = require('axios');
+
+exports.handler = async (event, context, callback) => {
+    try {
+        console.log('-------------');
+        const response = await axios.post('https://mnh51cvt0m.execute-api.sa-east-1.amazonaws.com/prod/odontofarma',
+            event.body
+        );
+        console.log(event.body);
+        console.log(response);
+        console.log('-------------');
+        console.log(response.data);
+        if (response.data.statusCode === 500) throw ('MSG COM FORMATO INVÁLIDO')
+        return callback(null, {
+            statusCode: 200,
+            body: JSON.stringify({
+                data: `Test data added successfully`
+            })
+        })
+    } catch (error) {
+        console.error(error);
+        return callback(null, {
+            statusCode: 501,
+            body: JSON.stringify({
+                data: `Erro no envio`
+            })
+        })
+    }
+}
